5854a7a8aae4d9cf9a23895988e93b8a4426ad9c
HEE-JE <windba78@naver.com>
2022-07-14 16:26:14 +0900

    jsx tutorial #2

--- 1.basics/ex08/src/02/App.js
+++ 1.basics/ex08/src/02/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
 
-function App(props) {
+function App() {
+  /*
+        리액트 컴포넌트는 단일 루트 노드만 렌더링 할수 있다.
+        오류:
+        return (
+            <h2>App02</h2>
+            <p>JSX Tutorial - 특징2: Single Root Node</p>
+        )
+    */
   return (
-    <div>
-      
+    <div id="App">
+      <h2>App02</h2>
+      <p>JSX Tutorial - 특징2: Single Root Node</p>
     </div>
   );
 }
