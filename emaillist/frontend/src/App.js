d4d16504db798edfdabe105f49cd2e7fc87e72b9
HEE-JE <windba78@naver.com>
2022-07-15 16:30:24 +0900

    emaillist #1

---
+++ emaillist/frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import emails from "./assets/json/data.json";
+
+function App() {
+  console.log(emails);
+  return (
+    <div id="App">
+      <h1>emaillist</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ emaillist/readme.md
@@ -0,0 +1,5 @@
+# React Practices - Integration(개발통합환경)
+
+## emaillist
+
+### Configuration
