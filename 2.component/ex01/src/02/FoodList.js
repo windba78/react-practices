ef2521e668d113d31fa27ad928c52c6031dfdc78
HEE-JE <windba78@naver.com>
2022-07-15 12:22:17 +0900

    component-props #2

---
+++ 2.component/ex01/src/02/FoodList.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import FoodListItem from "./FoodListItem";
+
+class FoodList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    // const components = [];
+    // this.props.foods.forEach((food) => {
+    //   components.push(
+    //     <FoodListItem name={food.name} quantity={food.quantity} />
+    //   );
+    // });
+
+    // const result = [1, 2, 3, 4].map(function (e) {
+    //   return e * e;
+    // });
+    // console.log(result);
+
+    return (
+      <div>
+        <ul>
+          {this.props.foods.map((food) => (
+            <FoodListItem
+              key={food.no}
+              name={food.name}
+              quantity={food.quantity}
+            />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default FoodList;
---
+++ 2.component/ex01/src/03/FoodListItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FoodListItem({ name, quantity }) {
+  return (
+    <li>
+      {name}: {quantity}
+    </li>
+  );
+}
+
+export default FoodListItem;
