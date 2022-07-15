f5f5dc4f7c468499e89ac53b03b65599e9e896cd
HEE-JE <windba78@naver.com>
2022-07-15 11:45:35 +0900

    component-props #1

---
+++ 2.component/ex01/src/01/FoodListItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class FoodListItem extends Component {
+  render() {
+    return (
+      <li>
+        {this.props.name}: {this.props.quantity}
+      </li>
+    );
+  }
+}
+
+export default FoodListItem;
