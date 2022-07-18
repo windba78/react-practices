e74619574c512eb881ffff63454d06d0191790e9
HEE-JE <windba78@naver.com>
2022-07-18 10:18:44 +0900

    Component Styling #3

---
+++ 2.component/ex02/src/04/Banner01.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./assets/css/Banner01.css";
+
+function Banner01() {
+  return <h1 className={styles.Header}>Hello React</h1>;
+}
+
+export default Banner01;
