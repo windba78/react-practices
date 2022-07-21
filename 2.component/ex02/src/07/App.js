a3235812cbbb10701ee016be49cd1f2eab2e92a0
HEE-JE <windba78@naver.com>
2022-07-21 10:55:57 +0900

    fontawesome #finish

--- 2.component/ex02/src/07/App.js
+++ 2.component/ex02/src/07/App.js
@@ -5,9 +5,13 @@ import {
   faCheckCircle,
   faTimesCircle,
 } from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 
 // additional
 function App() {
+  library.add(faBell, far, fab);
   return (
     <div style={{ color: "red" }}>
       {/* example */}
@@ -16,10 +20,17 @@ function App() {
       <FontAwesomeIcon icon={faTimesCircle} />
 
       {/* soild */}
+      <FontAwesomeIcon icon={["fas", "bell"]} />
+      <FontAwesomeIcon icon={"bell"} />
 
       {/* regular */}
+      <FontAwesomeIcon icon={["far", "bell"]} />
 
       {/* brands */}
+      <FontAwesomeIcon icon={["fab", "apple"]} />
+      <FontAwesomeIcon icon={["fab", "github"]} />
+      <FontAwesomeIcon icon={["fab", "google"]} />
+      <FontAwesomeIcon icon={["fab", "facebook"]} />
     </div>
   );
 }
