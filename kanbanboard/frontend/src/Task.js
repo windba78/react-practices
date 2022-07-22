ec55a2a416432aeb4e1a920cfdb25d41d822c1f8
HEE-JE <windba78@naver.com>
2022-07-22 18:03:41 +0900

    kanbanboard #3

--- kanbanboard/frontend/src/Task.js
+++ kanbanboard/frontend/src/Task.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import styles from "./assets/scss/Task.scss";
 
 export default function Task({ no, name, done, notifyChangeTaskDone }) {
+  const [checked, setChecked] = useState(false);
+
   return (
     <li className={styles.Task}>
-      <input type="checkbox" checked={done === "Y"} />
+      <input
+        type="checkbox"
+        checked={checked}
+        onChange={(e) => setChecked((checked) => !checked)}
+      />
       {name}
       <a href="#" className={styles.Task__remove} />
     </li>
