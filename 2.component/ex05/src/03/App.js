a10550286e3a80bca709cfad98a5cdcb0f2742af
HEE-JE <windba78@naver.com>
2022-07-25 12:22:28 +0900

    Component Lifecycle #2

--- 2.component/ex05/src/03/App.js
+++ 2.component/ex05/src/03/App.js
@@ -1,18 +1,29 @@
-import React, { Component } from 'react';
-import './assets/scss/App.scss'
-import Clock from './Clock';
+import React, { Component } from "react";
+import "./assets/scss/App.scss";
+import Clock from "./Clock";
 
 export default class App extends Component {
-    constructor() {
-        super(...arguments);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      hours: 12,
+      minutes: 12,
+      seconds: 30,
+      session: "am",
+    };
+  }
 
-    render() {
-        return (
-            <div className='clock-display'>
-                <h2>ex05 - Component LifeCycle Practice</h2>
-                    <Clock />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="clock-display">
+        <h2>ex05 - Component LifeCycle Practice</h2>
+        <Clock
+          hours={this.state.hours}
+          minutes={this.state.minutes}
+          seconds={this.state.seconds}
+          session={this.state.session}
+        />
+      </div>
+    );
+  }
 }
