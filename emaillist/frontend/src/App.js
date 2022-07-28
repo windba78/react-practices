ced5ea9d6964b8386df6d239570dc2e423d7be81
HEE-JE <windba78@naver.com>
2022-07-28 12:06:13 +0900

    fetch api #2

--- emaillist/frontend/src/App.js
+++ emaillist/frontend/src/App.js
@@ -7,28 +7,38 @@ import Emaillist from "./Emaillist";
 const App = () => {
   const [emails, setEmails] = useState([]);
 
-  const notifyEmailDelete = (no) => {
+  const emailDelete = (no) => {
     console.log(no);
   };
 
-  const notifyEmailAdd = (email) => {
-    console.log(email);
-  };
-
-  const notifyKeywordChanged = (keyword) => {
-    const result = data.filter(
-      (e) =>
-        e.firstName.indexOf(keyword) !== -1 ||
-        e.lastName.indexOf(keyword) !== -1 ||
-        e.email.indexOf(keyword) !== -1
-    );
-
-    setEmails(result);
-  };
-
-  useEffect(async () => {
+  const emailAdd = async (email) => {
     try {
       const response = await fetch("/api", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(email),
+      });
+
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+
+      const json = await response.json();
+      if (json.result !== "success") {
+        throw new Error(`${json.result} ${json.message}`);
+      }
+      setEmails([json.data, ...emails]);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const keywordChanged = async (keyword) => {
+    try {
+      const response = await fetch(`/api?kw=${keyword}`, {
         method: "get",
         headers: {
           "Content-Type": "application/json",
@@ -49,13 +59,41 @@ const App = () => {
     } catch (err) {
       console.error(err);
     }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api", {
+          method: "get",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          body: null,
+        });
+
+        if (!response.ok) {
+          throw new Error(`${response.status} ${response.statusText}`);
+        }
+
+        const json = await response.json();
+        if (json.result !== "success") {
+          throw new Error(`${json.result} ${json.message}`);
+        }
+        setEmails(json.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchData();
   }, []);
 
   return (
     <div id={"App"}>
-      <RegisterForm callback={notifyEmailAdd} />
-      <SearchBar callback={notifyKeywordChanged} />
-      <Emaillist emails={emails} callback={notifyEmailDelete} />
+      <RegisterForm callback={emailAdd} />
+      <SearchBar callback={keywordChanged} />
+      <Emaillist emails={emails} callback={emailDelete} />
     </div>
   );
 };
