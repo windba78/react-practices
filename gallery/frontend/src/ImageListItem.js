780fadb3348706973b648c79dc82fbe2145e7782
HEE-JE <windba78@naver.com>
2022-08-01 11:26:33 +0900

    gallery #finish

---
+++ gallery/frontend/src/ImageListItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./assets/scss/ImageListItem.scss";
+
+export default function ImageListItem({ no, url, comment, callback }) {
+  return (
+    <li className={styles.ImageListItem}>
+      <span
+        style={{
+          backgroundImage: `url(${url})`,
+        }}
+      />
+      <a onClick={() => callback(no)}>삭제</a>
+    </li>
+  );
+}
